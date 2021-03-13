b5bb856d0e7cbcdcc4927b067a42a95cb115ea01
Sameer Sam <sameerpokharel29@gmail.com>
2021-03-13 23:01:46 +0545

    committed

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
-// import { Button } from 'antd';
-import './App.css';
-import { useEffect,useState } from 'react';
-import Navbar from './Components/Navbar';
-import Userlist from './Components/Userlist';
+import './App.less'
+import Writerdetails from './Components/Body/Writerdetails';
 
+
+import Navbar from './Components/Header/Navbar';
 function App() {
-  const [click,setClick]= useState(false)
-  const [resourceType, setresourceType] = useState('')
-  const [items, setItems] = useState([])
-  
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-  .then(response => response.json())
-  .then(json => setItems(json))
-    
-  },[resourceType])
-
- 
-  
   return (
-    <div className="App">
-     <Navbar resourceType={resourceType} setresourceType={setresourceType} items={items} click={click} 
-     setClick={setClick} /> 
-      {click &&  items.map(item=>{
-      return (
-      <Userlist item={item} key={item.userId}/>
-      ) 
-     })
-}
-     </div>  
+    <>
+    <Navbar />
+    <Writerdetails />
+
+     </>  
       );
 }
-
 export default App;
